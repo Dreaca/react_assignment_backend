a346264065965f4d4e7f1d2d542606e60c869b5d
dreaca <buddhikadreaca@gmail.com>
2025-02-10 10:26:39 +0530

    added setup.bat file to easily configure the project

---
+++ setup.bat
@@ -0,0 +1,43 @@
+tsc --init
+npm init
+
+REM to install prisma
+npm install prisma typescript tsx @types/node --save-dev
+
+REM to install nodemon
+npm i -g nodemon
+
+REM to install ts-node (to work with nodemon)
+npm i -g ts-node
+
+REM install express
+npm i typescript @types/node @types/express express
+
+REM install cors
+npm i cors
+npm i @types/cors
+
+REM install axios
+npm i axios
+
+REM install dotenv
+npm i dotenv
+npm i @types/dotenv
+
+REM install bcrypt
+npm i bcrypt
+npm i @types/bcrypt
+
+REM initialize prisma
+npx prisma init
+
+REM install prisma client
+npm i @prisma/client
+
+REM generate prisma schema
+npx i prisma generate
+
+REM push your models in schema to the database
+REM npx prisma db push
+
+
